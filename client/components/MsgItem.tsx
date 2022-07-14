69b085bc7f15ca486b23f62f208945f751666e09
HyejinUm <uhj1993@naver.com>
2022-07-14 14:45:30 +0900

    Feat: #2 MsgItem Component

---
+++ client/components/MsgItem.tsx
@@ -0,0 +1,20 @@
+const MsgItem = ({ userId, timestamp, text }) => (
+  <ul className="messages__item">
+    <h3>
+      {userId}{" "}
+      <sub>
+        {new Date(timestamp).toLocaleString("ko-KR", {
+          year: "numeric",
+          month: "numeric",
+          day: "numeric",
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: true,
+        })}
+      </sub>
+    </h3>
+    {text}
+  </ul>
+);
+
+export default MsgItem;
