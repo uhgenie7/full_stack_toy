727a005902a3db34e7b9a4712da576cc2454592e
HyejinUm <uhj1993@naver.com>
2022-07-14 15:52:52 +0900

    Fix: #6 server-rendered 해결

--- client/components/MsgList.tsx
+++ client/components/MsgList.tsx
@@ -1,7 +1,6 @@
-import MsgItem from "./MsgItem";
+import { useEffect, useState } from "react";
 
-const UserIds = ["roy", "jay"];
-const getRandomUserId = () => UserIds[Math.round(Math.random())];
+import MsgItem from "./MsgItem";
 
 // const msgs = [
 //   {
@@ -12,21 +11,40 @@ const getRandomUserId = () => UserIds[Math.round(Math.random())];
 //   },
 // ];
 
-const msgs = Array(50)
-  .fill(0)
-  .map((_, i) => ({
-    id: 50 - i,
-    userId: getRandomUserId(),
-    timestamp: 1234567890123 + (50 - i) * 1000 * 60,
-    text: `${50 - i} mock text`,
-  }));
+interface Message {
+  id: number;
+  userId: string;
+  timestamp: number;
+  text: string;
+}
 
-const MsgList = () => (
-  <ul className="messages">
-    {msgs.map((x) => (
-      <MsgItem key={x.id} {...x} />
-    ))}
-  </ul>
-);
+const MsgList = () => {
+  const UserIds = ["roy", "jay"];
+
+  const [msgs, setMsgs] = useState<Message[]>([]);
+
+  useEffect(() => {
+    const getRandomUserId = (): string => UserIds[Math.round(Math.random())];
+
+    const settingMsg = Array(50)
+      .fill(0)
+      .map((_, i) => ({
+        id: 50 - i,
+        userId: getRandomUserId(),
+        timestamp: 1234567890123 + (50 - i) * 1000 * 60,
+        text: `${50 - i} mock text`,
+      }));
+
+    setMsgs(settingMsg);
+  }, []);
+
+  return (
+    <ul className="messages">
+      {msgs.map((x) => (
+        <MsgItem key={x.id} {...x} />
+      ))}
+    </ul>
+  );
+};
 
 export default MsgList;
