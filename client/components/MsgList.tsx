0f5a3a01bf5f3b22648cc4ad3b282d1411136ba1
HyejinUm <uhj1993@naver.com>
2022-07-14 15:00:52 +0900

    Refactor: #2 msgs 난수 효과적으로 만들기

--- client/components/MsgList.tsx
+++ client/components/MsgList.tsx
@@ -3,19 +3,28 @@ import MsgItem from "./MsgItem";
 const UserIds = ["roy", "jay"];
 const getRandomUserId = () => UserIds[Math.round(Math.random())];
 
-const msgs = [
-  {
-    id: 1,
+// const msgs = [
+//   {
+//     id: 1,
+//     userId: getRandomUserId(),
+//     timestamp: 1234567890123,
+//     text: "1 mock text",
+//   },
+// ];
+
+const msgs = Array(50)
+  .fill(0)
+  .map((_, i) => ({
+    id: i + 1,
     userId: getRandomUserId(),
-    timestamp: 1234567890123,
-    text: "1 mock text",
-  },
-];
+    timestamp: 1234567890123 + i * 1000 * 60,
+    text: `${i + 1} mock text`,
+  }));
 
 const MsgList = () => (
   <ul className="messages">
-    {[].map((x) => (
-      <MsgItem {...x} />
+    {msgs.map((x) => (
+      <MsgItem key={x.id} {...x} />
     ))}
   </ul>
 );
