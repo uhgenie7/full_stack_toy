904bfec5ff94a46947978e6cc6efda03bab13e59
HyejinUm <uhj1993@naver.com>
2022-07-15 11:41:34 +0900

    Feat: #8 express setup to index.js

---
+++ server/src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+app.listen(8000, () => {
+  console.log("server listen");
+});
