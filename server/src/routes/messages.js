82c384b1c613379cd19c337b7863c5d125e9a9a1
HyejinUm <uhj1993@naver.com>
2022-07-15 13:27:24 +0900

    Feat: #8 update method handler

--- server/src/routes/messages.js
+++ server/src/routes/messages.js
@@ -9,7 +9,7 @@ const messagesRoute = [
     method: "get",
     route: "/messages",
     handler: (req, res) => {
-      const msgs = getMsgs;
+      const msgs = getMsgs();
       res.send(msgs);
     },
   },
@@ -18,7 +18,7 @@ const messagesRoute = [
     method: "post",
     route: "/messages",
     handler: ({ body, params, query }, res) => {
-      const msgs = getMsgs;
+      const msgs = getMsgs();
       const newMsgs = {
         id: v4(),
         text: body.text,
@@ -34,9 +34,21 @@ const messagesRoute = [
     // UPDATE MESSAGE
     method: "put",
     route: "/messages/:id",
-    handler: (req, res) => {
-      const msgs = getMsgs;
-      res.send();
+    handler: ({ body, parmas: { id } }, res) => {
+      try {
+        const msgs = getMsgs();
+        const targetIndex = msgs.findIndex((msg) => msg.id === id);
+        if (targetIndex < 0) throw "메시지가 없습니다.";
+        if (msgs[targetIndex].userId !== body.userId)
+          throw "사용자가 다릅니다.";
+
+        const newMsg = { ...msgs[targetIndex], text: body.text };
+        msgs.splice(targetIndex, 1, newMsg);
+        setMsgs(msgs);
+        res.send(newMsg);
+      } catch (err) {
+        res.status(500).send({ error: err });
+      }
     },
   },
   {
@@ -44,7 +56,7 @@ const messagesRoute = [
     method: "delete",
     route: "/messages/:id",
     handler: (req, res) => {
-      const msgs = getMsgs;
+      const msgs = getMsgs();
       res.send();
     },
   },
