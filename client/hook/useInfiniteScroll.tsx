1cba3ade30212121b1b8fa16482a43fa090309eb
HyejinUm <uhj1993@naver.com>
2022-07-18 15:57:52 +0900

    Feat: #10 무한스크롤 hook 기능 개선하기
    observerRef 최초 init 이후에, 저장된 observerRef.current를 return 한다.

--- client/hook/useInfiniteScroll.tsx
+++ client/hook/useInfiniteScroll.tsx
@@ -1,17 +1,27 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback, useRef } from "react";
 
 const useInfiniteScroll = (targetEl) => {
+  const observerRef = useRef(null);
   const [intersecting, setIntersecting] = useState(false);
 
-  const observer = new IntersectionObserver((entries) =>
-    setIntersecting(entries.some((entry) => entry.isIntersecting))
-  );
+  //   const observer = new IntersectionObserver((entries) =>
+  //     setIntersecting(entries.some((entry) => entry.isIntersecting))
+  //   );
+
+  const getObserver = useCallback(() => {
+    if (!observerRef.current) {
+      observerRef.current = new IntersectionObserver((entries) =>
+        setIntersecting(entries.some((entry) => entry.isIntersecting))
+      );
+    }
+    return observerRef.current;
+  }, [observerRef.current]);
 
   useEffect(() => {
-    if (targetEl.current) observer.observe(targetEl.current);
+    if (targetEl.current) getObserver().observe(targetEl.current);
 
     return () => {
-      observer.disconnect();
+      getObserver().disconnect();
     };
   }, [targetEl.current]);
 
