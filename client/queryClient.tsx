7ef0df21c5a06214c8e6e3f03e5e46c204607445
HyejinUm <uhj1993@naver.com>
2022-07-20 10:52:16 +0900

    Fix: #16 variables default 값 주기

--- client/queryClient.tsx
+++ client/queryClient.tsx
@@ -1,4 +1,6 @@
 import { request } from "graphql-request";
+
 const URL = "http://localhost:8000/graphql";
 
-export const fetcher = (query, variables) => request(URL, query, variables);
+export const fetcher = (query, variables = {}) =>
+  request(URL, query, variables);
