4115f802a3f98a9cba62e6d45934ae64e0e5f5b6
HyejinUm <uhj1993@naver.com>
2022-07-20 14:32:43 +0900

    Feat: #17 QueryKeys 작성하기

--- client/queryClient.tsx
+++ client/queryClient.tsx
@@ -4,3 +4,10 @@ const URL = "http://localhost:8000/graphql";
 
 export const fetcher = (query, variables = {}) =>
   request(URL, query, variables);
+
+export const QueryKeys = {
+  MESSAGES: "MESSAGES",
+  MESSAGE: "MESSAGE",
+  USERS: "USERS",
+  USER: "USER",
+};
