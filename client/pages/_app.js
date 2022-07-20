68feb75753953eca9a74171ec96457c3617a37e9
HyejinUm <uhj1993@naver.com>
2022-07-20 10:09:46 +0900

    Feat: #16 react-query 초기화

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,14 @@
 import "./index.scss";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-const App = ({ Component, pageProps }) => <Component {...pageProps} />;
+const App = ({ Component, pageProps }) => {
+  const QueryClient = new QueryClient()
+  return (
+    <QueryClientProvider client={}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
+};
 
 App.getInitialProps = async ({ ctx, Component }) => {
   const pageProps = await Component.getInitialProps?.(ctx);
