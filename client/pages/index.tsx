93e0651fa58fd35bbd8ba09e13845d3d9f7e24b6
HyejinUm <uhj1993@naver.com>
2022-07-20 13:50:06 +0900

    Doc: #17 비구조화 할당 확인

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,21 +1,24 @@
 import MsgList from "../components/MsgList";
 import { fetcher } from "../queryClient";
-import { GET_MESSAGES } from "../graphql/messages";
+import { GET_MESSAGES } from "../graphql/message";
 import { GET_USERS } from "../graphql/user";
 
 const Home = ({ smsgs, users }) => (
   <>
-    <h1>home</h1>
+    <h1>SIMPLE SNS</h1>
     <MsgList smsgs={smsgs} users={users} />
   </>
 );
 
 export const getServerSideProps = async () => {
-  const smsgs = await fetcher(GET_MESSAGES);
-  const users = await fetcher(GET_USERS);
+  // const smsgs  = await fetcher(GET_MESSAGES);
+  const { messages: smsgs } = await fetcher(GET_MESSAGES);
+  const { users } = await fetcher(GET_USERS);
+
   console.log({ smsgs, users });
+
   return {
-    props: { smsgs: [], users: {} },
+    props: { smsgs, users },
   };
 };
 
