75775ceda9a826e2a77b6b0217d7259d009a6adc
HyejinUm <uhj1993@naver.com>
2022-07-22 10:35:10 +0900

    Feat: #20 dbController에 lowdb 사용

--- server/src/dbController.js
+++ server/src/dbController.js
@@ -1,7 +1,8 @@
-// 원래 nodejs는 es6 모듈 문법을 사용할 수 없음. package.json을 설정해주어야 함
-// const fs = require("fs");
-
 // import fs from "fs";
 // import { resolve } from "path";
+
 import { LowSync, JSONFileSync } from "lowdb";
 const adapther = new JSONFileSync("/src/db.json");
+const db = new LowSync(adapther);
+
+export default db;
