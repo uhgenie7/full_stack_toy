6ed1f276ad43a6c24389737321068d628d0b10b2
HyejinUm <uhj1993@naver.com>
2022-08-03 18:34:12 +0900

    feat: SSR

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,16 +1,19 @@
-import MsgList from "../components/MsgList";
-import fetcher from "../fetcher";
+import MsgList from "../src/components/UI/MsgList";
+import MsgListFetcher from "../src/components/Fetcher/MsgListFetcher";
+import fetcher from "../src/api";
+import { METHOD, Message, Users } from "../src/types";
 
-const Home = ({ smsgs, users }) => (
+const Home = ({ smsgs, users }: { smsgs: Message[]; users: Users }) => (
   <>
     <h1>home</h1>
-    <MsgList smsgs={smsgs} users={users} />
+    <MsgListFetcher smsgs={smsgs} users={users} />
   </>
 );
 
 export const getServerSideProps = async () => {
-  const smsgs = await fetcher("get", "/messages");
-  const users = await fetcher("get", "/users");
+  const smsgs = await fetcher(METHOD.GET, "/messages");
+  const users = await fetcher(METHOD.GET, "/users");
+
   return {
     props: { smsgs, users },
   };
