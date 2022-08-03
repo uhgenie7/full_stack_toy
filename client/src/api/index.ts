a1be4089933903d9377bc890bc52d594b7123d74
HyejinUm <uhj1993@naver.com>
2022-08-03 18:34:25 +0900

    feat: API 정리

---
+++ client/src/api/index.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { METHOD } from "../types";
+
+axios.defaults.baseURL = "http://localhost:8000";
+
+const fetcher = async (
+  method: METHOD,
+  url: string,
+  ...rest: { [key: string]: any }[]
+) => {
+  const res = await axios[method](url, ...rest);
+  return res.data;
+};
+
+export default fetcher;
+
+/*
+get: axios.get(url[, config])
+delete: axios.delete(url[, config])
+post: axios.post(url, data[, config])
+put: axios.put(url, data[, config])
+*/
